e544744f0d1a6b7f28e7d9e42a139beef6eeed82
frikstobal <cristobalbahamonde@hotmail.com>
2018-11-11 03:09:17 -0300

    main Problem Solved *wrapper pending

--- src/life.cpp
+++ src/life.cpp
@@ -18,103 +18,79 @@
 
 using namespace std;
 
-void createGrid(Grid<bool>& colony, string Fname, int& rows, int& columns);
-void GridUpdate(Grid<bool>& colony, Grid<bool> colonyCopy);
+void createGrid(Grid<bool>& colony, Grid<bool>& colonyCopy, string Fname, int& rows, int& columns);
+void GridUpdate(Grid<bool>& colony, Grid<bool>& colonyCopy);
 void animateGrid(const Grid<bool>& colony , int rows, int columns , LifeGUI& gui );
-bool lookForNeighbours(int& x, int& y);
-int upperLeft(int x ,int y);
-int up(int x ,int y);
-int upperRight(int x ,int y);
-int left(int x ,int y);
-int right(int x ,int y);
-int lowerLeft(int x ,int y);
-int down(int x ,int y);
-int lowerRight(int x ,int y);
+bool lookForNeighbours(int& x, int& y, const Grid<bool>& colony);
+int upperLeft(int x ,int y, const Grid<bool>& colony);
+int up(int x ,int y, const Grid<bool>& colony);
+int upperRight(int x ,int y, const Grid<bool>& colony);
+int left(int x ,int y, const Grid<bool>& colony);
+int right(int x ,int y, const Grid<bool>& colony);
+int lowerLeft(int x ,int y, const Grid<bool>& colony);
+int down(int x ,int y, const Grid<bool>& colony);
+int lowerRight(int x ,int y, const Grid<bool>& colony);
 
 
 int main() {
 
-
     Grid<bool> colony;    //Assigned Grid depending on the original file
-
     Grid<bool> colonyCopy; //Buffer for processed data
-
-    LifeGUI gui;       // GUI objects constructor
-
+    LifeGUI gui;       // GUI constructor
     string filename;
+    bool wrapOption;
+    char action;
+    int rows;
+    int columns;
+
+    createGrid(colony, colonyCopy, filename, rows, columns);
 
 
+    while(true){
+        animateGrid(colony, rows, columns, gui);
+        GridUpdate(colony, colonyCopy);
 
-        bool wrapOption;
-
-        char action;
-
-        int rows;
-
-        int columns;
-
-
-
-
-
-    createGrid(colony, filename, rows, columns);
-
-    animateGrid(colony, rows, columns, gui);
-
+    }
+    //cout << " Colony " << colony << endl;
+    //cout << " ColonyCopy " << colonyCopy << endl;
     wrapOption = getYesOrNo("do you want to wrap around the cell world? Yes | No ");
-
     cout << "a | animate - t | tick - q | quit" << endl;
-
     action = getchar();
-
     return 0;
 
 }
 
 //***** this method creates the grid according to the input file*****
 
- void createGrid(Grid<bool>& colony, string Fname, int& rows, int& columns){
+ void createGrid(Grid<bool>& colony, Grid<bool>& colonyCopy, string Fname, int& rows, int& columns){
 
      int deadCell = '-';                                                                       //dead cell token to read
-
      int aliveCell = 'X';                                                                      // alive cell token
-
      ifstream stream;                                                                          //input stream for file
-
      string newLine;                                                                          //line that's read from the input file, used as a buffer
 
       cout << "Please select the action that you want to perform." << endl;
-
             Fname = getLine("Please enter the filename.");                                     //we store the name of the file in Fname string
 
             stream.open(Fname);                                                                //we open the file **adjustment needed --> loop nd file verification
-
             stream >> rows;                                                                    //we store the number of rows and columns specified from the file
-
             stream >> columns;
-
             colony.resize( rows, columns );                                                    // with both variables declared we resize the grid
-
+            colonyCopy.resize( rows, columns );
 
 
             for(int i = 0; i < rows; i++){
-
                 stream >> newLine;                                                            // we pick every line here
 
                 for(int j = 0; j < columns; j++){                                             // and scan each one of the characters
-
                     bool value;                                                               // this boolean is used to store the status of the character
-
                     int token = newLine[j];                                                   // current scanned character
 
                     if(token == deadCell)                                                     //here we compare if it's a dead or an alive cell
-
                         value = false;
 
                         else if(token == aliveCell) {
-
                             value = true;
-
                             }
 
                     colony[i][j] = value;                                                 //we add the boolean value to the grid
@@ -123,25 +99,27 @@ int main() {
             cout << colony << endl;                                                       //***********************+++DEBUG
 
 
-
 }
 //**********************************This method takes care of the grid update according to the colony rules*****************************************************************+
 
  void GridUpdate(Grid<bool>& colony, Grid<bool>& colonyCopy ){
 
-     int cellX = 2;
-     int cellY = 3;
+     int gridRowsY = 0;
+     int gridColumnsX = 1;
+ //    cout<< " x " << gridColumnsX << " y " << gridRowsY << " <- coordinates "  << endl;
+     lookForNeighbours(gridColumnsX, gridRowsY, colony);
+     int maxX = colony.nCols;
+     int maxY = colony.nRows;
 
-     lookForNeighbours(cellX, cellY);
-
-
-     int maxX = colony.nRows;
-     int maxY = colony.nCols;
-     for(int Xpos; Xpos < maxX; Xpos++){
-         for(int Ypos; Ypos < maxY; Ypos++){
+     for(int Ypos = 0; Ypos < maxY; Ypos++){
+         for(int Xpos = 0; Xpos < maxX; Xpos++){
+           bool cellState = lookForNeighbours(Ypos, Xpos, colony);
+           colonyCopy[Ypos][Xpos] = cellState;
 
          }
      }
+
+     colony = colonyCopy;
 
  }
 
@@ -149,6 +127,7 @@ int main() {
 
 
 //************************++++HERE WE UPDATE THE GUI*************************************************************************************************************************
+
 
  void animateGrid(const Grid<bool>& colony ,int rows, int columns , LifeGUI& theGUI){
 
@@ -167,81 +146,147 @@ int main() {
 
  //**************Scans each one of the neighbours of the cell and storages the alues in a vector*****
 
- bool lookForNeighbours(int& x, int& y, const vector<bool>& colony ){
+ bool lookForNeighbours(int& x, int& y, const Grid<bool>& colony ){
+
+    //cout << "neighBours" << endl;
+
     Vector<int> locations[8];    // locations of all cells get storaged here
     locations->insert( 0,
-                       upperLeft(x , y));  //scan first position and store in the array, the same with all the other values up to the lower left cell
+                       upperLeft(x , y, colony));  //scan first position and store in the array, the same with all the other values up to the lower left cell
     locations->insert( 1,
-                       up(x , y));
+                       up(x , y, colony));
     locations->insert( 2,
-                       upperRight(x , y));
+                       upperRight(x , y, colony));
     locations->insert( 3,
-                       left(x , y));
+                       left(x , y, colony));
     locations->insert( 4,
-                       right(x , y));
+                       right(x , y, colony));
     locations->insert( 5,
-                       lowerLeft(x , y));
+                       lowerLeft(x , y, colony));
     locations->insert( 6,
-                       down(x , y));
+                       down(x , y, colony));
     locations->insert( 7,
-                       lowerRight(x , y));
+                       lowerRight(x , y, colony));
         int numberOfNeighbours = 0;                                                                                        // variable to store all the neighbours
         for(int count = 0; count < locations->size(); count++){                                                            //loop through the lovation values
                                                                     int cellValue = locations->get(count);
                                                                     numberOfNeighbours = numberOfNeighbours + cellValue;   //we add the values progressively
                                                                }
-        if(numberOfNeighbours >= 3){ return true;                                                                          //if there are more than three neighbours, the cell is alive
+     //  cout << "current cell Value: " << colony[x][y] << endl;
+        if(numberOfNeighbours == 3){
+
+                                    return true;                                                                          //if there are more than three neighbours, the cell is alive
                                                         }
+            else if(numberOfNeighbours == 2 && colony[x][y] == true){
+                             return true;
+                             }
+
+                else if(numberOfNeighbours > 4){
+                                     return false;
+                                     }
+
+
                             else{   return false;                                                                          //if not, then is dead
                                                         }
  }
 
 
- int upperLeft(int x ,int y, const Grid<bool>& colony){                                                                     //need to optimize this redundant code
-     bool isAlive = colony.get(x,y);
-     if(isAlive)return 1;
-         else return 0;
- }
+ int lowerLeft(int x ,int y, const Grid<bool>& colony){                                                                    //need to optimize this redundant code (?)
+    int cellScanX = x - 1;
+    int cellScanY = y + 1;
+     if (colony.inBounds( cellScanX, cellScanY)){
+      bool isAlive = colony.get(cellScanX ,cellScanY);
+       // debug  cout << cellScanX  << " y " << cellScanY << " present " << isAlive << endl;
+         if(isAlive)return 1;
+             else return 0;
+    }
+    else {return 0;
+          cout << "cell not present "  << endl;
+           }
 
- int up(int x ,int y, const Grid<bool>& colony){
-     bool isAlive = colony.get(x,y);
-     if(isAlive)return 1;
-         else return 0;
- }
-
- int upperRight(int x ,int y, const Grid<bool>& colony){
-     bool isAlive = colony.get(x,y);
-     if(isAlive)return 1;
-         else return 0;
- }
-
- int left(int x ,int y, const Grid<bool>& colony){
-     bool isAlive = colony.get(x,y);
-     if(isAlive)return 1;
-         else return 0;
- }
-
- int right(int x ,int y, const Grid<bool>& colony){
-     bool isAlive = colony.get(x,y);
-     if(isAlive)return 1;
-         else return 0;
- }
-
- int lowerLeft(int x ,int y, const Grid<bool>& colony){
-     bool isAlive = colony.get(x,y);
-     if(isAlive) return 1;
-         else return 0;
  }
 
  int down(int x ,int y, const Grid<bool>& colony){
-     bool isAlive = colony.get(x,y);
-     if(isAlive) return 1;
+     int cellScanX = x ;
+     int cellScanY = y + 1;
+      if (colony.inBounds( cellScanX, cellScanY)){
+     bool isAlive = colony.get(cellScanX ,cellScanY);
+    // cout << cellScanX  << " y " << cellScanY << " present " << isAlive << endl;
+     if(isAlive)return 1;
          else return 0;
+     }
+      else {return 0;}
  }
 
  int lowerRight(int x ,int y, const Grid<bool>& colony){
-     bool isAlive = colony.get(x,y);
+     int cellScanX = x + 1;
+     int cellScanY = y + 1;
+      if (colony.inBounds( cellScanX, cellScanY)){
+     bool isAlive = colony.get(cellScanX, cellScanY);
+    // cout << cellScanX  << " y " << cellScanY << " present " << isAlive << endl;
+     if(isAlive)return 1;
+         else return 0;
+     }
+      else {return 0;}
+ }
+
+ int left(int x ,int y, const Grid<bool>& colony){
+     int cellScanX = x - 1;
+     int cellScanY = y;
+      if (colony.inBounds( cellScanX, cellScanY)){
+     bool isAlive = colony.get(cellScanX, cellScanY);
+     //cout << cellScanX  << " y " << cellScanY << " present " << isAlive << endl;
+     if(isAlive)return 1;
+         else return 0;
+     }
+      else {return 0;}
+ }
+
+ int right(int x ,int y, const Grid<bool>& colony){
+     int cellScanX = x + 1;
+     int cellScanY = y;
+      if (colony.inBounds( cellScanX, cellScanY)){
+     bool isAlive = colony.get(cellScanX, cellScanY);
+    // cout << cellScanX  << " y " << cellScanY << " present " << isAlive << endl;
+     if(isAlive)return 1;
+         else return 0;
+     }
+      else {return 0;}
+ }
+
+ int upperLeft(int x ,int y, const Grid<bool>& colony){
+     int cellScanX = x - 1;
+     int cellScanY = y - 1;
+      if (colony.inBounds( cellScanX, cellScanY)){
+     bool isAlive = colony.get(cellScanX, cellScanY);
+    // cout << cellScanX  << " y " << cellScanY << " present " << isAlive << endl;
+     if(isAlive) return 1;
+         else return 0;
+     }
+      else {return 0;}
+ }
+
+ int up(int x ,int y, const Grid<bool>& colony){
+     int cellScanX = x;
+     int cellScanY = y - 1;
+      if (colony.inBounds( cellScanX, cellScanY)){
+     bool isAlive = colony.get(cellScanX, cellScanY);
+    // cout << cellScanX  << " y " << cellScanY << " present " << isAlive << endl;
+     if(isAlive) return 1;
+         else return 0;
+     }
+      else {return 0;}
+ }
+
+ int upperRight(int x ,int y, const Grid<bool>& colony){
+     int cellScanX = x + 1;
+     int cellScanY = y - 1;
+      if (colony.inBounds( cellScanX, cellScanY)){
+     bool isAlive = colony.get(cellScanX, cellScanY);
+    // cout << cellScanX  << " y " << cellScanY << " present " << isAlive << endl;
      if(isAlive)
          return 1;
          else return 0;
+     }
+      else {return 0;}
  }
